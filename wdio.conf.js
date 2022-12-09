5a24ea9603ba6e9f34e740c8b2d1e04a8fffbf5b
zahid ahmed <zahidahmed1256@gmail.com>
2022-12-09 16:38:02 -0500

    crossbrowser testing

--- wdio.conf.js
+++ wdio.conf.js
@@ -57,14 +57,19 @@ exports.config = {
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
         maxInstances: 5,
-        //
         browserName: 'chrome',
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
+    },
+    {
+        maxInstances: 5,
+        browserName: 'firefox',
+        acceptInsecureCerts: true
     }],
+
     //
     // ===================
     // Test Configurations
@@ -112,7 +117,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['selenium-standalone'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
