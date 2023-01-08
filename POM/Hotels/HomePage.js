7a62d8f3bab23f9ad64129ce4231d363da6fabc4
zahid ahmed <zahidahmed1256@gmail.com>
2023-01-08 00:23:16 -0500

    project

--- POM/Hotels/HomePage.js
+++ POM/Hotels/HomePage.js
@@ -9,11 +9,29 @@ class HomePage {
     goingToLocator = '//button[@aria-label="Going to"]';
     goingToTypeLocator = '#destination_form_field';
     autoSuggestionsLocator = '//div[@class="truncate"]//strong';
-    languageLocator = '//div[text()="English"]'
+    languageLocator = '//button[@data-stid]//div[contains(@class, "uitk-text-default-theme")]'
     languageDropDownLocator = '#language-selector'
     spanishLocator = '//option[text()="Español (Estados Unidos)"]'
     englishLocator = '//option[text()="English (United States)"]'
-    saveLocator = '//button[text()="Save]'
+    saveButtonLocator = '//button[text()="Save" or text()="Guardar"]'
+    datesLocator = '//button[@id="date_form_field-btn"]'
+    getTheAppLocator = '//button[text()="Get the app"]'
+    phoneNumberLocator = '//input[@name="phoneNumber"]'
+    phoneNumberErrorMsgLocator = '//div[text()="Please enter a valid phone number."]'
+
+    //Travelers
+    travelersField = '//button[contains(@class, "uitk-menu-trigger uitk-fake-input uitk-form-field-trigger")]'
+    adultsIncrementButton = '//*[@aria-label="Increase the number of adults in room 1"]'
+    childIncrementButton = '//*[@aria-label="Increase the number of children in room 1"]'
+    decreaseAdultCountLocator= '//*[@aria-label="Decrease the number of adults in room 1"]'
+    decreaseChildCountLocator= '//*[@aria-label="Decrease the number of children in room 1"]'
+
+
+
+
+
+
+
 
 
     // Calendar
@@ -26,6 +44,24 @@ class HomePage {
     nextCalendarButtonLocator = '(//button[@data-stid="date-picker-paging"])[2]';
     prevCalendarButtonLocator = '(//button[@data-stid="date-picker-paging"])[1]';
     leftSideCalendarHeaderLocator = '(//div[@class="uitk-date-picker-month"])[1]//h2';
+    pastDisabledDatesLocator = '//button[contains(@aria-label, "disabled")]'
+    backMonthButtonLocator = '(//button[@data-stid="date-picker-paging"])[1]'
+
+
+    //Sign in Locators
+
+    signInButtonLocator = '//button[text()="Sign in"]'
+    feedbackLinkLocator = '//a[text()="Feedback"]'
+    submitButtonLocator = '#submit-button'
+    starBoxErrorLocator = '//div[@class="radio-group"]'
+    errorMsgLocator ='//div[@id="validation_header"]//p[contains(text(), "Please fill in")]'
+    starRatingLocator = '//label[@for="page-rating-4"]'
+    commentLocator = '//textarea[@name="verbatim"]'
+    likelyToReturnLocator= '//select[@id="will-you-return"]'
+    feedbackConfirmationLocator= '//h5[@class="question-header"]'
+
+    //
+
 
     // functions to interact with the web-Elements on the HomePage
     async enterDestination(destination) {
@@ -91,8 +127,103 @@ class HomePage {
         return await this.commands.isWebElementEnabled(this.englishLocator)
     }
 
-    
+    async clickOnDate() {
+        await this.commands.clickWebElement(this.datesLocator)
+    }
 
+
+    async findDisabledDates() {
+        return await this.commands.findAllWebElement(this.pastDisabledDatesLocator)
+    }
+
+    async clickSignInButton() {
+        await this.commands.clickWebElement(this.signInButtonLocator)
+    }
+
+    async clickFeedbackLink() {
+        await this.commands.clickWebElement(this.feedbackLinkLocator)
+    }
+
+    async switchToNextWindowHandle() {
+        const hotelWindowHandle = await this.commands.getHandle()
+        const allWindowHandles = await this.commands.getHandles()
+        for (const handle of allWindowHandles) {
+            if (handle !== hotelWindowHandle) {
+                await this.commands.switchToWindowHandle(handle)
+                break;
+            }
+        }
+
+    }
+
+    async clickSubmitButton() {
+        await this.commands.clickWebElement(this.submitButtonLocator)
+    }
+
+    async scrollToGetTheApp() {
+        await this.commands.scrollIntoView(this.getTheAppLocator);
+        await browser.pause(2000)
+    }
+
+    async clickPhoneNumber() {
+        await this.commands.clickWebElement(this.phoneNumberLocator)
+    }
+
+    async enterPhoneNumber (phone) {
+        await this.commands.typeInWebElement(this.phoneNumberLocator,phone)
+    }
+
+    async clickGetTheApp() {
+        await this.commands.clickWebElement(this.getTheAppLocator)
+    }
+
+    async getPhoneErrorText() {
+        return await this.commands.getTextOfWebElement(this.phoneNumberErrorMsgLocator)
+    }
+
+    async clickOnTravelers() {
+        await this.commands.clickWebElement(this.travelersField)
+    }
+
+
+    async changeLanguage(newLanguage) {
+        await this.commands.clickWebElement(this.languageLocator)
+        await this.commands.selectDataInDropdown(this.languageDropDownLocator, newLanguage)
+        await this.commands.clickWebElement(this.saveButtonLocator)
+    }
+
+    async getLanguageFromWeb() {
+        return await this.commands.getTextOfWebElement(this.languageLocator)
+    }
+
+    async getErrorText() {
+        return await this.commands.getTextOfWebElement(this.errorMsgLocator)
+    }
+
+    async isStarErrorDisplayed() {
+        await this.commands.isWebElementDisplayed(this.starBoxErrorLocator)
+    }
+
+    async getCurrentMonthAndYear() {
+        await this.commands.getTextOfWebElement(this.leftSideCalendarHeaderLocator)
+    }
+
+    async clickStarRating() {
+        await this.commands.clickWebElement(this.starRatingLocator)
+    }
+
+    async enterComment(comment) {
+        await this.commands.clickWebElement(this.commentLocator)
+        await this.commands.typeInWebElement(this.commentLocator,comment)
+    }
+
+    async selectLikelyToReturnDropdown(choice) {
+        await this.commands.selectDataInDropdown(this.likelyToReturnLocator, choice)
+    }
+
+    async isFeedbackConfirmationDisplayed() {
+        await this.commands.isWebElementDisplayed(this.feedbackConfirmationLocator)
+    }
 
     // 'May 2023'
     async goToMonth(monthYear) {
